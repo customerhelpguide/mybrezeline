3ac216a035e2da15d1eb0cc2065496251017bbbc
customerhelpguide <michaelsrya012@outlook.com>
2026-04-18 11:12:28 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -2,22 +2,30 @@ project = 'breezeline'
 author = 'breezeline'
 release = '1.0'
 
-extensions = []
-templates_path = ['_templates']
-exclude_patterns = []
-
-html_theme = 'alabaster'
-html_static_path = ['_static']   
-
-html_js_files = [
-    'chatbot.js',
-]
-html_favicon = '_static/favicon.png'
-
-html_context = {
-    'bing_verification_code': '739245F5D54BCBF40AC056DC0CBF5710'
-}
-
+# Extensions
 extensions = [
     'sphinx_sitemap',
 ]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
+
+# Google & Bing Verification Meta Tags
+html_context = {
+    "meta_tags": """
+    <meta name="google-site-verification" content="Cva8KgvW-eQpRtsdf8vIcSb023IJtLJfC8PxJAlQ0mc" />
+    <meta name="msvalidate.01" content="739245F5D54BCBF40AC056DC0CBF5710" />
+    """
+}
+
+# Base URL for sitemap
+html_baseurl = 'https://breezeline.readthedocs.io/en/latest/index.html'
