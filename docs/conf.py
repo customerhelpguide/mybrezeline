125a6daafa8bfe603c296a3eb96302c133d331ca
customerhelpguide <michaelsrya012@outlook.com>
2026-04-18 11:05:20 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,23 @@
+project = 'breezeline'
+author = 'breezeline'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']   
+
+html_js_files = [
+    'chatbot.js',
+]
+html_favicon = '_static/favicon.png'
+
+html_context = {
+    'bing_verification_code': '739245F5D54BCBF40AC056DC0CBF5710'
+}
+
+extensions = [
+    'sphinx_sitemap',
+]
